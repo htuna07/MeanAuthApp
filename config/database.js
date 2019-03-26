a461134ea9185d867444ba254245f7f75b734d0a
Tuna <h.tuna96@hotmail.com>
2019-03-27 02:31:50 +0300

    thats it

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 
 //specify database properties
 module.exports = {
-    database : 'mongodb+srv://htuna07:<190796>@cluster0-5jmrt.mongodb.net/test?retryWrites=true',
+    database : 'mongodb+srv://htuna07:190796@cluster0-5jmrt.mongodb.net/test?retryWrites=true',
     secret : 'somethingsecret'
     
 }
